3fe5d6162009d2fac2025c665ea0bfedc1fe4475
Jonnycp <jonathan-caputo@hotmail.com>
2022-10-21 07:34:31 +0200

    Sistemata lettura da file con getCode

--- CodiceFiscale/static/php/readFile.php
+++ CodiceFiscale/static/php/readFile.php
@@ -1,12 +1,10 @@
 <?php
     function readCsv() {
         $file = fopen("static/data/codiciCatastali.csv", "r");
-        $luoghiNascita = array();
+        $luoghiNascita = [];
         while(!feof($file)) {
             $luogoNascita = fgets($file);
-            echo $luogoNascita;
-            //array_push($luogoNascita, $luogoNascita);
-            print($luoghiNascita);
+            array_push($luoghiNascita, trim($luogoNascita));
         }
         fclose($file);
         return $luoghiNascita;
@@ -14,9 +12,20 @@
 
     function csv2Datalist() {
         $luoghiNascita = readCsv();
-        print_r($luoghiNascita);
         foreach($luoghiNascita as $luogoNascita) {
-            echo "<option value='trim(".$luogoNascita[0].")'>".trim($luogoNascita[0])."</option>";
+            $luogoNascita = explode(";", $luogoNascita);
+            //RALLENTA PERFORMANCE... meglio JS
+            echo "<option value='".$luogoNascita[0]."'>".trim($luogoNascita[0])."</option>";
+        }
+    }
+
+    function getCode($citta) {
+        $luoghiNascita = readCsv();
+        foreach($luoghiNascita as $luogoNascita) {
+            $luogoNascita = explode(";", $luogoNascita);
+            if(cleanInputs($luogoNascita[0]) === $citta) {
+                return $luogoNascita[2];
+            }
         }
     }
 ?>
